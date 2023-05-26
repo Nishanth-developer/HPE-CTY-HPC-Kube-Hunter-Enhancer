c7c1efe4761af08fcd3a89659a958671e13fa775
Nishanth VM <72375481+Nishanth-developer@users.noreply.github.com>
2023-05-26 11:07:49 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,30 +1,3 @@
-#
-# #!/bin/bash
-#
-# # Check if Git is installed and install it if not
-# if ! command -v git &> /dev/null; then
-#     echo "Git not found. Installing Git..."
-#     sudo apt-get update
-#     sudo apt-get install git -y
-# fi
-#
-# # Create the target directory in Documents folder
-# mkdir -p ~/Documents/target_directory
-#
-# # Clone the private repository
-# git clone https://github.com/Nishanth-developer/HPE-CTY-2023-Kube-Hunter.git
-#
-# # Copy the contents of kube-hunter-enhancer directory to the target directory
-# cp -r HPE-CTY-2023-Kube-Hunter/kube-hunter-enhancer/* ~/Documents/target_directory/
-#
-# # Navigate to the app directory
-# cd ~/Documents/target_directory
-#
-# # Install the necessary packages using pip
-# pip install -r requirements.txt
-#
-# # Run the main.py file
-# python3 main.py
 
 #!/bin/bash
 
@@ -39,10 +12,10 @@ fi
 mkdir -p ~/Documents/target_directory
 
 # Clone the private repository
-git clone https://github.com/Nishanth-developer/HPE-CTY-2023-Kube-Hunter.git
+git clone https://github.com/Nishanth-developer/HPE-CTY-HPC-Kube-Hunter-Enhancer.git
 
 # Copy the contents of kube-hunter-enhancer directory to the target directory
-cp -r HPE-CTY-2023-Kube-Hunter/kube-hunter-enhancer/* ~/Documents/target_directory/
+cp -r HPE-CTY-HPC-Kube-Hunter-Enhancer/kube-hunter-enhancer/* ~/Documents/target_directory/
 
 # Navigate to the app directory
 cd ~/Documents/target_directory
